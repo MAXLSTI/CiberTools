042135c8dab71812e07ddb04f9fc598f00a52392
JaunRuizz <73042090+jaunRuizz@users.noreply.github.com>
2021-09-29 18:25:18 -0500

    Se agrego funcion para enviar correos

---
+++ 29-09-2021/CiberTools-main/main.py
@@ -0,0 +1,18 @@
+from analisis import *
+
+
+if  __name__=="__main__":
+   #enviarcorreo()
+   ##ip_public=my_ip()
+   #ip, domain = geolocalizacion()
+   #if ip.ip_local is None:
+   #   print("Error no ingresaste el parametro -k usa --help para mas informacion ")
+   #   exit()
+   #else:
+   #   print("Iniciando scaneo ")
+   #   scan(ip) 
+   #   Ports()
+      
+      
+    
+   
